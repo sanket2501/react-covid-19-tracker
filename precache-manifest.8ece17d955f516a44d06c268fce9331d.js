352315c65b9b6a31d8da50eb2a597b6bc59a79b6
sanket <sanketshrawagi86999@gmail.com>
2021-09-02 18:06:16 +0530

    Updates

---
+++ precache-manifest.8ece17d955f516a44d06c268fce9331d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e85a2a196036251343aca6c4c9ec2a3",
+    "url": "/react-covid-19-tracker/index.html"
+  },
+  {
+    "revision": "2a96b07a2fa03d18b5aa",
+    "url": "/react-covid-19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "d7ac606dbe636a5932fc",
+    "url": "/react-covid-19-tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "2a96b07a2fa03d18b5aa",
+    "url": "/react-covid-19-tracker/static/js/2.0a983f0d.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/react-covid-19-tracker/static/js/2.0a983f0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7ac606dbe636a5932fc",
+    "url": "/react-covid-19-tracker/static/js/main.ebb5135c.chunk.js"
+  },
+  {
+    "revision": "049b208a1648ca99347c",
+    "url": "/react-covid-19-tracker/static/js/runtime-main.73ff399f.js"
+  }
+]);
